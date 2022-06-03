2ade8f7c113a9ff756b46ecf25e0edf83a71a722
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-06-03 18:22:50 -0300

    Aplicativo finalizado.

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import Cep from './components/Cep';
+import Api from './components/Api';
 
 export default function App() {
+  const [cep, setCep] = useState("");
+  const [inputCep, setInputCep] = useState(0);
+
+  async function buscaCep(){
+    const response = await Api.get('ws/'+inputCep+'/json/');
+    setCep(response.data);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.bloco}>
-        <Text style={styles.text}> Digite seu CPF:</Text>
+        <Text style={styles.text}> Digite seu CEP:</Text>
         
         <TextInput 
         placeholder='ex: 11730-000' 
         keyboardType= 'numeric' 
-        style={styles.input}/>
+        style={styles.input}
+        onChangeText={(data)=>setInputCep(data)}/>
 
-     <TouchableOpacity style={styles.botao}> 
+      <TouchableOpacity
+      style={styles.botao}
+      onPress={buscaCep}> 
           <Text style={styles.txtBotao}> Buscar</Text>
-          </TouchableOpacity>
+      </TouchableOpacity>
 
-          <Cep/>
+          <Cep data={cep} />
       </View>
     </View>
   );
